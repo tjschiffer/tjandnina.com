2e97e7d195de02e8ae04d8b44b184264a573f448
T.J. Schiffer <tjschiffer@gmail.com>
2018-05-17 06:51:27 -0400

    move output to root and start landing page

--- build/makeSite.js
+++ build/makeSite.js
@@ -5,20 +5,20 @@ const fs = require('fs');
 
 const templateDirPath = path.resolve('../templates');
 const templateDataPath = path.resolve('../templates/languageData.json');
-const outputFolder = path.resolve('../output/');
+const outputFolder = path.resolve('../');
 
 generator.render(templateDirPath, templateDataPath, outputFolder, err => {
   if (err) throw err;
 });
 
 const sassFile = path.resolve('../scss/main.scss');
-const outFile = path.resolve('../output/css/main.css');
+const outFile = path.resolve('../css/main.css');
 
 sass.render({
   file: sassFile,
   sourceMap: true,
   outFile: outFile,
-  outputStyle: 'compressed'
+  outputStyle: 'nested'
 }, function(err, result) {
   if (err) throw err;
   fs.writeFile(outFile, result.css, err => {
