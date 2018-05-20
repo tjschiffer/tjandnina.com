8c64ddfb329f42ed570d62555a70ef971a7e5b4b
Unknown <tjschiffer@gmail.com>
2018-05-20 19:52:53 -0400

    Update makeSite for new pages

--- makeSite.js
+++ makeSite.js
@@ -2,10 +2,17 @@ const generator = require('multi-language-site-generator');
 const sass = require('node-sass');
 const path = require('path');
 const fs = require('fs');
+const async = require('async');
 
 const layoutComponent = new (require('./components/layout-component/layout-component'))();
 const headComponent = new (require('./components/head-component/head-component'))();
-const navigationComponent = new (require('./components/navigation-component/navigation-component'))()
+const navigationComponent = new (require('./components/navigation-component/navigation-component'))();
+const footerComponent = new (require('./components/footer-component/footer-component'))();
+
+const heroImageComponent = new (require('./components/hero-image-component/hero-image-component'))();
+const detailsComponent = new (require('./components/details-component/details-component'))();
+const registryComponent = new (require('./components/registry-component/registry-component'))();
+const photosComponent = new (require('./components/photos-component/photos-component'))();
 
 function renderLanguages() {
   const templateDirPath = path.resolve('./templates');
@@ -17,27 +24,93 @@ function renderLanguages() {
   });
 }
 
-// Render landing page
-const landingPageComponent = new (require('./components/landing-page-component/landing-page-component.js'))();
+// Generate each page of the site and save the template to be rendered for each language
+// Use series so that singleton components do not clash
+async.series([
+    cb => {
+      layoutComponent
+        .setHeadComponent(headComponent)
+        .setContentComponent(heroImageComponent)
+        .setNavigationComponent(navigationComponent.setIsOverlay(true))
+        .setFooterComponent(footerComponent)
+        .render((err, renderedTemplate) => {
+          if (err) {
+            cb(err);
+            return;
+          }
+          fs.writeFile('./templates/index.mustache', renderedTemplate, err => {
+            cb(err);
+          });
+        });
+    },
+    cb => {
+      layoutComponent
+        .setHeadComponent(headComponent.setTitle('T.J. & Nina | {{ Details }}'))
+        .setContentComponent(detailsComponent)
+        .setNavigationComponent(navigationComponent.setIsOverlay(false))
+        .setFooterComponent(footerComponent)
+        .render((err, renderedTemplate) => {
+          if (err) {
+            cb(err);
+            return;
+          }
+          fs.writeFile('./templates/details.mustache', renderedTemplate, err => {
+            cb(err);
+          });
+        });
+    },
+    cb => {
+      layoutComponent
+        .setHeadComponent(headComponent.setTitle('T.J. & Nina | {{ Registry }}'))
+        .setContentComponent(registryComponent)
+        .setNavigationComponent(navigationComponent.setIsOverlay(false))
+        .setFooterComponent(footerComponent)
+        .render((err, renderedTemplate) => {
+          if (err) {
+            cb(err);
+            return;
+          }
+          fs.writeFile('./templates/registry.mustache', renderedTemplate, err => {
+            cb(err);
+          });
+        });
+    },
+    cb => {
+      layoutComponent
+        .setHeadComponent(headComponent.setTitle('T.J. & Nina | {{ Photos }}'))
+        .setContentComponent(photosComponent)
+        .setNavigationComponent(navigationComponent.setIsOverlay(false))
+        .setFooterComponent(footerComponent)
+        .render((err, renderedTemplate) => {
+          if (err) {
+            cb(err);
+            return;
+          }
+          fs.writeFile('./templates/photos.mustache', renderedTemplate, err => {
+            cb(err);
+          });
+        });
+    }
+  ],
+  (err) => {
+    if(err) throw err;
+    renderLanguages();
+  }
+);
+
+// Render landing page with no header or footer
+const landingPageComponent = new (require('./components/language-chooser-component/language-chooser-component'))();
 layoutComponent
   .setHeadComponent(headComponent)
   .setContentComponent(landingPageComponent)
+  .setNavigationComponent(null)
+  .setFooterComponent(null)
   .render((err, renderedTemplate) => {
     if (err) throw err;
     fs.writeFile('./index1.html', renderedTemplate, err => {
       if(err) throw err;
     });
   });
-
-// Generate each page of the site and save the template to be rendered for each language
-navigationComponent.render((err, renderedTemplate) => {
-  if (err) throw err;
-  console.log(renderedTemplate);
-  fs.writeFile('./index1.html', renderedTemplate, err => {
-    if(err) throw err;
-  });
-});
-
 
 // Generate css
 const sassFile = path.resolve('./scss/main.scss');
