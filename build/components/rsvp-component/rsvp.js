d9c89966bfd6b7b8fd51b7af22faff779d70cf40
T.J. Schiffer <tjschiffer@gmail.com>
2018-09-27 23:31:00 +0000

    google sheets api

--- build/components/rsvp-component/rsvp.js
+++ build/components/rsvp-component/rsvp.js
@@ -5,7 +5,6 @@ import options from '../../auth.json'
 
 const contacts = new LoadGoogleAPI(options);
 
-
 export default (el) => {
   el.querySelectorAll('[data-tj-rsvp]').forEach((rsvp) => {
     console.log(contacts);
@@ -17,7 +16,7 @@ export default (el) => {
       },
       methods: {
         findRsvp() {
-
+            return 'https://sheets.googleapis.com/v4/spreadsheets/1TIGZJn0ZPDhVjTXFBfIIXrPIQs8pCf20CqRNaCrLvc4/values/Sheet1!A1:D5';
         }
       }
     });
--- build/main.js
+++ build/main.js
@@ -1,5 +1,7 @@
-import navigation from './components/navigation-component/navigation.js';
+import navigation from './components/navigation-component/navigation';
+import rsvp from './components/rsvp-component/rsvp';
 
 window.onload = () => {
   navigation(document.body);
+  rsvp(document.body);
 };
