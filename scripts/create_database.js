dc84b809dfae81bf67bdbcdd88001ccfe7ed2a87
Unknown <tjschiffer@gmail.com>
2018-10-01 22:40:22 -0400

    import data

--- scripts/create_database.js
+++ scripts/create_database.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const dbconfig = require('../config/database');
+const fs = require('fs');
 
 const createDatabase = async() => {
   const connectionConfig = Object.assign({}, dbconfig.connection);
@@ -23,57 +24,78 @@ const createDatabase = async() => {
     VALUES \
     (1, \'' + dbconfig.app.user + '\', \'' + dbconfig.app.password_hash + '\')',
 
-    'CREATE TABLE `' + dbconfig.connection.database + '`.`' + dbconfig.rsvp_table + '` ( \
+    'CREATE TABLE `' + dbconfig.connection.database + '`.`' + dbconfig.invites_table + '` ( \
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
     `zip_code` VARCHAR(20) NOT NULL, \
+    `invite_welcome_event` BOOLEAN NOT NULL, \
+    `invite_after_party` BOOLEAN NOT NULL, \
     `note` TEXT, \
         PRIMARY KEY (`id`), \
     UNIQUE INDEX `id_UNIQUE` (`id` ASC))',
 
     'CREATE TABLE `' + dbconfig.connection.database + '`.`' + dbconfig.guests_table + '` ( \
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
-    `rsvp_id` INT UNSIGNED NOT NULL, \
+    `invite_id` INT UNSIGNED NOT NULL, \
     `first_name` VARCHAR(30) NOT NULL, \
     `last_name` VARCHAR(30) NOT NULL, \
     `attending` BOOLEAN, \
     `attending_welcome_event` BOOLEAN, \
     `attending_after_party` BOOLEAN, \
+    `timestamp` TIMESTAMP NOT NULL, \
         PRIMARY KEY (`id`), \
     UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
-    FOREIGN KEY (rsvp_id) \
-    REFERENCES `tjandnina`.rsvp(id) \
+    FOREIGN KEY (`invite_id`) \
+    REFERENCES `tjandnina`.invite(id) \
     ON UPDATE CASCADE ON DELETE RESTRICT)',
 
     'CREATE TABLE `' + dbconfig.connection.database + '`.`' + dbconfig.guests_history_table + '` ( \
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
     `guests_id` INT UNSIGNED NOT NULL, \
-    `rsvp_id` INT UNSIGNED NOT NULL, \
+    `invite_id` INT UNSIGNED NOT NULL, \
     `first_name` VARCHAR(30) NOT NULL, \
     `last_name` VARCHAR(30) NOT NULL, \
     `attending` BOOLEAN, \
     `attending_welcome_event` BOOLEAN, \
     `attending_after_party` BOOLEAN, \
+    `timestamp` TIMESTAMP NOT NULL, \
         PRIMARY KEY (`id`), \
     UNIQUE INDEX `id_UNIQUE` (`id` ASC))'
   ];
 
   // Drop the database if the argument "--drop" is present
   if (process.argv.slice(2).indexOf('--drop') > -1) {
-    queries.unshift('DROP DATABASE IF EXISTS `' + dbconfig.connection.database + '`' + '\n');
+    queries.unshift('DROP DATABASE IF EXISTS `' + dbconfig.connection.database + '`');
   }
 
-  for (const query of queries) {
-    console.log('Executing statement: ' + query);
-    const executedQuery = await new Promise(resolve => {
-      connection.query(query, err => {
-        if (err) {
-          resolve(err);
-        } else {
-          resolve('Success!');
-        }
-      });
+  const inviteData = await new Promise(resolve => {
+    fs.readFile('./config/invites.csv', 'utf-8', (err, data) => {
+      const insertValues = data.replace(new RegExp(',,', 'g'), ',NULL,').split('\r\n').slice(1, data.length - 1).join('),\r\n(');
+      resolve('(' + insertValues + ')');
     });
-    console.log('Result: ' + executedQuery+ '\n');
+  });
+
+  queries.push('INSERT INTO `' + dbconfig.connection.database + '`.`' + dbconfig.invites_table + '` \
+    (`id`, `zip_code`, `invite_welcome_event`, `invite_after_party`) \
+    VALUES \
+    ' + inviteData);
+
+
+  for (const query of queries) {
+    if (process.argv.slice(2).indexOf('--dry-run=false') > -1) {
+      console.log('Executing statement: ' + query);
+      const executedQuery = await new Promise(resolve => {
+        connection.query(query, err => {
+          if (err) {
+            resolve(err);
+          } else {
+            resolve('Success!');
+          }
+        });
+      });
+      console.log('Result: ' + executedQuery + '\n');
+    } else {
+      console.log('Dry run statement: ' + query + '\n');
+    }
   }
 
   connection.end();
