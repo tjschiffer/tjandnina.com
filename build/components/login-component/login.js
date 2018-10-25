5739073830c2f26f865d828273f0c1d0c6e7b66c
Unknown <tjschiffer@gmail.com>
2018-10-24 23:22:57 -0400

    rename vue

--- build/components/login-component/login.js
+++ build/components/login-component/login.js
@@ -1,9 +1,9 @@
-import vue from 'vue'
+import Vue from 'vue'
 import loginApp from './login.vue'
 
 export default () => {
   document.querySelectorAll('[data-tj-login]').forEach((login) => {
-    new vue({
+    new Vue({
       el: login,
       render: h => h(loginApp)
     });
--- build/main.js
+++ build/main.js
@@ -1,9 +1,11 @@
 import navigation from './components/navigation-component/navigation';
 import rsvp from './components/rsvp-component/rsvp';
 import login from './components/login-component/login';
+import invites from './components/invites-component/invites';
 
 window.onload = () => {
   navigation();
   rsvp();
   login();
+  invites();
 };
